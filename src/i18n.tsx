75845b15ef916bf3a2d3f277c17b5966f458ed49
tunogya <tunogya@qq.com>
2021-09-24 20:51:02 +0800

    fix i18n

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,8 +1,41 @@
-import { I18nProvider } from "@lingui/react"
-import { i18n } from "@lingui/core"
-import { ReactNode } from "react"
+import { I18nProvider } from '@lingui/react'
+import { i18n } from '@lingui/core'
+import { ReactNode, useEffect } from 'react'
+
+import { en, zh } from 'make-plural/plurals'
+import { detect, fromUrl, fromStorage, fromNavigator } from '@lingui/detect-locale'
+
+export const locales = {
+  "en-US": "English",
+  "zh-CN": "简体中文",
+};
+
+const DEFAULT_FALLBACK = () => 'en-US'
+export const defaultLocale = detect(fromUrl('lang'), fromStorage('lang'), fromNavigator(), DEFAULT_FALLBACK) || 'en-US'
+
+i18n.loadLocaleData({
+  "en-US": { plurals: en },
+  "zh-CN": { plurals: zh },
+})
+
+export async function dynamicActivate(locale: string) {
+  if (!(locale in locales)) {
+    locale = 'en-US'
+  }
+  const { messages } = await import(`./locales/${locale}.po`)
+  i18n.load(locale, messages)
+  i18n.activate(locale)
+
+  if (window.localStorage) {
+    window.localStorage.setItem('lang', locale)
+  }
+}
 
 export const LanguageProvider = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    dynamicActivate(defaultLocale)
+  }, [])
+
   return (
     <I18nProvider forceRenderOnLocaleChange={false} i18n={i18n}>
       {children}
