c3b981fa9f5d09ebdd30c75962b485dac308ff53
tunogya <tunogya@qq.com>
2021-10-06 11:00:34 +0800

    add copy button

--- src/components/Navigation/FileListItem.tsx
+++ src/components/Navigation/FileListItem.tsx
@@ -1,23 +1,27 @@
 import {
   Button,
-  Heading,
+  Heading, IconButton,
   Link,
   Menu,
   MenuButton,
   MenuDivider,
   MenuItem,
-  MenuList,
+  MenuList, Stack,
   Text,
 } from "@chakra-ui/react";
 import {FC, useEffect, useState} from "react";
 import {useNFTStorage} from "../../hooks/useNFTStorage";
 import {PROCESSING} from "../../constants/status";
 import {parseCidToHttpUrl} from "../../utils/ipfs";
+import {shortenCid} from "../../utils/ipfs";
+import {CopyIcon, DeleteIcon, ExternalLinkIcon} from "@chakra-ui/icons";
+import {bytesToSize} from "../../utils";
+import {BsImage} from "react-icons/all";
 
 interface Props {
   store: { file: File, cid: string }
   onDelete: (name: string) => void
-  onSetCid: (name: string, newStore: {file: File, cid: string}) => void
+  onSetCid: (name: string, newStore: { file: File, cid: string }) => void
 }
 
 const FileListItem: FC<Props> = ({store, onDelete, onSetCid}) => {
@@ -47,24 +51,29 @@ const FileListItem: FC<Props> = ({store, onDelete, onSetCid}) => {
         <Text px={3} fontWeight={"bold"}>
           {store.file.name}
         </Text>
-        <Text px={3}>
+        <Text px={3} fontSize={"xs"}>
           {store.file.type}
         </Text>
-        <Text px={3}>
-          {store.file.size}
+        <Text px={3} fontSize={"xs"}>
+          {bytesToSize(store.file.size)}
         </Text>
-        <Text px={3}>
-          {result}
-        </Text>
+        <Stack direction={"row"} alignItems={"center"}>
+          <Text px={3} fontSize={"xs"}>
+            {shortenCid(result, 8)}
+          </Text>
+          <IconButton aria-label={"copy"} icon={<CopyIcon/>} size={"xs"} variant={"ghost"}/>
+        </Stack>
+
         <MenuDivider/>
-        { result !== "" && (
-          <MenuItem>
-            <Link href={parseCidToHttpUrl(result)} isExternal>
-              <Heading size={"sm"} fontWeight={"normal"}>View</Heading>
-            </Link>
+        <MenuItem icon={<BsImage/>}>
+          <Heading size={"sm"} fontWeight={"normal"}>As Cover</Heading>
+        </MenuItem>
+        {result !== "" && (
+          <MenuItem icon={<ExternalLinkIcon/>} as={Link} href={parseCidToHttpUrl(result)} isExternal>
+            <Heading size={"sm"} fontWeight={"normal"}>View</Heading>
           </MenuItem>
-        ) }
-        <MenuItem color={"red"} onClick={() => onDelete(store.file.name)}>
+        )}
+        <MenuItem color={"red"} onClick={() => onDelete(store.file.name)} icon={<DeleteIcon/>}>
           <Heading size={"sm"} fontWeight={"normal"}>Delete</Heading>
         </MenuItem>
       </MenuList>
--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,3 +1,8 @@
+export const shortenCid = (address: string, chars = 6) => {
+  if (!address) { return }
+  return `${address.substring(0, chars)}...${address.substring(address.length - chars)}`
+}
+
 export const parseCidToHttpUrl = (cid: string) => {
   if (cid === "") { return }
   return "https://" + cid + ".ipfs.dweb.link/"
