9a8f0cd12a72bdf5a50e3a4857ee63c175f406d4
tunogya <tunogya@qq.com>
2021-10-24 18:25:57 +0800

    update

--- src/components/Navigation/BottomNavigation.tsx
+++ src/components/Navigation/BottomNavigation.tsx
@@ -1,10 +1,18 @@
-import {Heading, Stack} from "@chakra-ui/react";
+import {IconButton, Stack, useColorMode} from "@chakra-ui/react";
 import Auth from "./Auth";
+import {MoonIcon, SunIcon} from "@chakra-ui/icons";
 
 export const BottomNavigation = () => {
+  const {colorMode, toggleColorMode} = useColorMode()
+
   return (
-    <Stack h={16} direction={"row"} alignItems={"center"} p={2} justifyContent={"space-between"}>
-      <Heading fontSize={"md"}>Cogito ergo sum</Heading>
+    <Stack h={16} direction={"row"} alignItems={"center"} p={2} justifyContent={"flex-end"}>
+      <IconButton
+        aria-label={"btn"}
+        variant={"outline"}
+        icon={colorMode === "dark" ? <MoonIcon/> : <SunIcon/>}
+        onClick={toggleColorMode}
+      />
       <Stack w={60}>
         <Auth/>
       </Stack>
