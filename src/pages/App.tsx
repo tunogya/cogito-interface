6c0ce94c654acffc07a637bd55d8b442b58429bb
tunogya <tunogya@qq.com>
2021-10-24 15:50:41 +0800

    update height and width

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,10 +10,10 @@ import {BottomNavigation} from "../components/Navigation/BottomNavigation";
 import {TopNavigation} from "../components/Navigation/TopNavigation";
 
 function App() {
-  const {width} = useWindowDimensions()
+  const {width, height} = useWindowDimensions()
 
   return (
-    <Stack h={"100vh"} w={"100vw"} alignItems={"center"} direction={"row"} spacing={0} justifyContent={"center"}>
+    <Stack h={height} w={width} alignItems={"center"} direction={"row"} spacing={0} justifyContent={"center"}>
       {width >= 640 && <Navigation/>}
       <Divider orientation="vertical"/>
       <Stack w={"600px"} h={"100%"} spacing={0}>
