e4a68d71a1ecbbf0082f1c618c5cb1b69e9e2974
tunogya <tunogya@qq.com>
2021-10-24 19:43:46 +0800

    update menu bg

--- src/components/Navigation/Auth.tsx
+++ src/components/Navigation/Auth.tsx
@@ -6,27 +6,28 @@ import {
   Menu,
   MenuButton,
   MenuItem,
-  MenuList,
+  MenuList, useColorMode,
 } from "@chakra-ui/react"
-import { t, Trans } from "@lingui/macro"
-import { useCurrentUser } from "../../hooks/useCurrentUser"
-import { ChevronDownIcon, CopyIcon, ExternalLinkIcon } from "@chakra-ui/icons"
-import { useClipboard } from "@chakra-ui/react"
-import { AiOutlineLogout } from "react-icons/all"
+import {t, Trans} from "@lingui/macro"
+import {useCurrentUser} from "../../hooks/useCurrentUser"
+import {ChevronDownIcon, CopyIcon, ExternalLinkIcon} from "@chakra-ui/icons"
+import {useClipboard} from "@chakra-ui/react"
+import {AiOutlineLogout} from "react-icons/all"
 import shortenCid from "../../utils/shortenCid"
 import useWindowDimensions from "../../hooks/useWindowDimensions"
 
 const Auth = () => {
-  const { user, logIn, logOut } = useCurrentUser()
-  const { width } = useWindowDimensions()
-  const { hasCopied, onCopy } = useClipboard(user.addr ?? "")
+  const {user, logIn, logOut} = useCurrentUser()
+  const {width} = useWindowDimensions()
+  const {hasCopied, onCopy} = useClipboard(user.addr ?? "")
+  const {colorMode} = useColorMode()
 
   return (
     <>
       {user.loggedIn ? (
         <Menu>
-          {width >= 1200 || width <= 640  ? (
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} fontWeight={"bold"} variant={"outline"} isFullWidth>
+          {width >= 1200 || width <= 640 ? (
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon/>} fontWeight={"bold"} variant={"outline"} isFullWidth>
               {hasCopied ? t`Copied!` : shortenCid(user.addr ?? "", 6)}
             </MenuButton>
           ) : (
@@ -35,19 +36,19 @@ const Auth = () => {
             </MenuButton>
           )}
 
-          <MenuList borderRadius={"xl"} padding={0}>
-            <MenuItem onClick={onCopy} icon={<CopyIcon />}>
+          <MenuList borderRadius={"xl"} padding={0} background={colorMode === "light" ? "white" : "black"}>
+            <MenuItem onClick={onCopy} icon={<CopyIcon/>}>
               <Heading fontSize={"md"} fontWeight={"normal"}>
                 <Trans>Copy Address</Trans>
               </Heading>
             </MenuItem>
-            <MenuItem icon={<ExternalLinkIcon />} as={Link} href={"https://www.baidu.com"} isExternal>
+            <MenuItem icon={<ExternalLinkIcon/>} as={Link} href={"https://www.baidu.com"} isExternal>
               <Heading fontSize={"md"} fontWeight={"normal"}>
                 <Trans>View on Explorer</Trans>
               </Heading>
             </MenuItem>
-            <Divider />
-            <MenuItem onClick={logOut} color={"red"} icon={<AiOutlineLogout />} fontWeight={"bold"}>
+            <Divider/>
+            <MenuItem onClick={logOut} color={"red"} icon={<AiOutlineLogout/>} fontWeight={"bold"}>
               <Heading fontSize={"md"} fontWeight={"normal"}>
                 <Trans>Log out</Trans>
               </Heading>
