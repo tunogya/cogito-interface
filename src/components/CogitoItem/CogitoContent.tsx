20ca47e4dd3ce27fa37935896e23a4568ecd1b58
tunogya <tunogya@qq.com>
2021-10-27 23:33:09 +0800

    update

--- src/components/CogitoItem/CogitoContent.tsx
+++ src/components/CogitoItem/CogitoContent.tsx
@@ -1,6 +1,6 @@
 import {FC, Key} from "react";
 import useCogitoTokenURI from "../../hooks/useCogitoTokenURI";
-import {AspectRatio, Stack, Text, Image, Link, Button, Tooltip} from "@chakra-ui/react";
+import {Stack, Text, Link, Button, Tooltip} from "@chakra-ui/react";
 import {parseDate} from "../../utils/parseDate";
 import parseUriToHttp from "../../utils/parseUriToHttp";
 import shortenCid from "../../utils/shortenCid";
@@ -17,17 +17,33 @@ const CogitoContent: FC<Props> = props => {
     <Stack>
       <Text>{cogito.cogito.text}</Text>
       <Stack direction={"row"}>
-      {cogito.cogito?.attachment?.media.map((media: any, index: Key) => (
-        <>
-          {media?.uri && (
-            <Button key={index} as={Link} href={parseUriToHttp(media.uri)[0]} isExternal size={"sm"}>
+        {cogito.cogito?.attachment?.media.map((media: any, index: Key) => {
+          if (!media.uri) {
+            return null
+          }
+          return (
+            <Button key={index} as={Link} href={parseUriToHttp(media.uri)[0]} isExternal size={"sm"}
+                    colorScheme={"pink"}>
               <Tooltip label={media?.name} borderRadius={"xl"}>
                 <Text>{shortenCid(media?.name, 10)}</Text>
               </Tooltip>
             </Button>
-          )}
-        </>
-      ))}
+          )
+        })}
+
+        {cogito.cogito?.attachment?.files?.map((file: any, index: Key) => {
+          if (!file.uri) {
+            return null
+          }
+          return (
+            <Button key={index} as={Link} href={parseUriToHttp(file.uri)[0]} isExternal size={"sm"}>
+              <Tooltip label={file?.name} borderRadius={"xl"}>
+                <Text>{shortenCid(file?.name, 10)}</Text>
+              </Tooltip>
+            </Button>
+          )
+        })}
+
       </Stack>
       <Text fontSize={"xs"} color={"gray"}>{parseDate(cogito.cogito.create_at)}</Text>
     </Stack>
