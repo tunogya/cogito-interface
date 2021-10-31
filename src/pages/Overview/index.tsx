259bbb0140224a49f889b7909249643250c89121
tunogya <tunogya@qq.com>
2021-11-01 00:15:39 +0800

    update overview

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,5 +1,5 @@
 import Content from "../../components/Content"
-import { Text } from "@chakra-ui/react"
+import {Spinner, Stack, Text} from "@chakra-ui/react"
 import useFlowBalance from "../../hooks/useFlowBalance"
 import useCurrentUser from "../../hooks/useCurrentUser"
 import { Suspense } from "react"
@@ -10,14 +10,29 @@ import useFlowStorage from "../../hooks/useFlowStorage"
 import bytesToSize from "../../utils/bytesToSize"
 
 const Overview = () => {
-  const { user } = useCurrentUser()
-  const flow = useFlowBalance(user.addr)
-  const supply = useCogitoSupply()
-  const storage = useFlowStorage(user.addr)
   const { width } = useWindowDimensions()
 
   return (
     <Content label={"Overview"} hasDivider hasTitle={width >= 640}>
+      <Suspense fallback={
+        <Stack p={4}>
+          <Spinner/>
+        </Stack>
+      }>
+        <OverviewContent/>
+      </Suspense>
+    </Content>
+  )
+}
+
+const OverviewContent = () => {
+  const { user } = useCurrentUser()
+  const flow = useFlowBalance(user.addr)
+  const supply = useCogitoSupply()
+  const storage = useFlowStorage(user.addr)
+
+  return (
+    <Stack>
       <Text>{user.addr}</Text>
       {flow.balance && <Text>Balance: {parseFlow(flow.balance)} FLOW</Text>}
       <Text>Cogito Supply: {supply.supply}</Text>
@@ -27,16 +42,8 @@ const Overview = () => {
           {((storage.storage.storageUsed / storage.storage.storageCapacity) * 100).toFixed(2)}% used
         </Text>
       )}
-    </Content>
+    </Stack>
   )
 }
 
-const WrappedOverview = () => {
-  return (
-    <Suspense fallback={<Text>Loading</Text>}>
-      <Overview />
-    </Suspense>
-  )
-}
-
-export default WrappedOverview
+export default Overview
